afee1f390fe04929bf70a6a58906cbc5bb492412
Trobot <flybaboo@hotmail.com>
2021-01-17 00:45:24 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <button onClick={changeText}>Нормально</button>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//
+//         </a>
+//         <p>{this.state.text}</p>
+//       </header>
+//     </div>
+//   );
+// }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Learn'
+    };
+  }
+
+  changeText = () => {
+    this.setState(prevState => ({
+      text: this.state.text+'Нормально',
+    }));
+  };
+
+  render (){
+      return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={this.changeText}>Нормально</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+
+          </a>
+          <p>{this.state.text}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
